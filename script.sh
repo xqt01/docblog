cbad5d9277297927d5ae8181bb307e1787774daa
huanruiz <huanruizhang@trip.com>
2023-08-10 20:29:09 +0800

    script rm extension

--- script.sh
+++ script.sh
@@ -9,6 +9,8 @@ sorted_file_names=$(ls "$folder_path" | sort -r)
 # 打开README文件，将文件名按顺序写入时间线标签下
 sed -i '/## Timeline/,$d' README.md
 echo "## Timeline" >> README.md
-for file_name in $sorted_file_names; do
-  echo "- [$file_name](./docs/$file_name)" >> README.md
+for file_name in $sorted_file_names;
+do
+  file_name_without_extension="${file_name%.*}"
+  echo "- [$file_name_without_extension](./docs/$file_name)" >> README.md
 done
