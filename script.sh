2350904a346a0123eb1afd5ceeb167fd09f8ba03
huanruiz <huanruizhang@trip.com>
2023-08-10 20:26:02 +0800

    init

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 指定文件夹路径
+folder_path="docs"
+
+# 获取文件夹中所有文件名，并按文件名进行降序排序
+sorted_file_names=$(ls "$folder_path" | sort -r)
+
+# 打开README文件，将文件名按顺序写入时间线标签下
+sed -i '/## Timeline/,$d' README.md
+echo "## Timeline" >> README.md
+for file_name in $sorted_file_names; do
+  echo "- [$file_name](./docs/$file_name)" >> README.md
+done
